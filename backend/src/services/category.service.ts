4585d8611fb74e181f4df1539e027e503250324a
KambalaFish <justdoitnikey@gmail.com>
2023-01-16 17:17:20 +0700

    Category service was modified to use ServiceBase class

--- backend/src/services/category.service.ts
+++ backend/src/services/category.service.ts
@@ -1,32 +1,5 @@
-import type { ICategory } from '@models/category.model';
-import { CategoryModel } from '@models/category.model';
-import type { FilterQuery, QueryOptions, UpdateQuery } from 'mongoose';
+import { BaseCRUDService } from '@services/ServiceBase';
+import { CategoryModel, ICategory } from '@models/category.model';
 
-const createCategory = async (input: ICategory) => {
-  const category = await CategoryModel.create(input);
-  return category.toJSON();
-};
-
-const findCategories = async (query: FilterQuery<ICategory> = {}) => {
-  return CategoryModel.find(query).lean();
-};
-
-const updateCategory = async (
-  query: FilterQuery<ICategory>,
-  update: UpdateQuery<ICategory>,
-  options: QueryOptions = {
-    new: true,
-    lean: true,
-  }
-) => {
-  return CategoryModel.findOneAndUpdate(query, update, options);
-};
-
-const deleteCategory = async (
-  query: FilterQuery<ICategory>,
-  options: QueryOptions = {}
-) => {
-  return CategoryModel.findOneAndDelete(query, options);
-};
-
-export { createCategory, findCategories, updateCategory, deleteCategory };
+const categoryService = new BaseCRUDService<ICategory>(CategoryModel);
+export { categoryService };
