0f780ce51308f14baba370ebd104063250280f5d
KambalaFish <justdoitnikey@gmail.com>
2023-01-16 17:23:48 +0700

    routes/index.ts was updated

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,15 @@
 import type { Express, Request, Response } from 'express';
 import { StatusCodes } from '@utils/statusCodes';
 import { categoryRouter } from '@routes/category.routes';
+import { productRouter } from '@routes/product.routes';
+
 const routes = (app: Express) => {
   app.get('/ping', (req: Request, res: Response) => {
     res.status(StatusCodes.OK).json({ message: 'pong' });
   });
 
-  app.use('/category', categoryRouter);
+  app.use('/categories', categoryRouter);
+  app.use('/products', productRouter);
 };
 
 export { routes };
