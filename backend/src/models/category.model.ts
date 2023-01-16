ad253f55524cda20c0e0c37d6586c75fce2113b3
KambalaFish <justdoitnikey@gmail.com>
2023-01-16 16:55:28 +0700

    category schema was modified

--- backend/src/models/category.model.ts
+++ backend/src/models/category.model.ts
@@ -3,12 +3,17 @@ export interface ICategory {
   name: string;
 }
 
-const categorySchema = new mongoose.Schema<ICategory>({
-  name: {
-    type: String,
-    required: true,
+const categorySchema = new mongoose.Schema<ICategory>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
   },
-});
+  {
+    strictQuery: 'throw',
+  }
+);
 
 const CategoryModel = mongoose.model<ICategory>('Category', categorySchema);
 
